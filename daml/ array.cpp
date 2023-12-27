c1898c420168dff9da1472686150f7ce5eb03512
Damien Rouchouse <damien.rouchouse@etu.minesparis.psl.eu>
2023-12-27 19:24:10 +0100

    starting project !

---
+++ daml/ array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include "array.h"
+
+using namespace std;
+
+array::array()
+{
+    cout << "default constructor" << endl;
+}
+
+array::array(const int size){
+    vector<int> size;
+}
---
+++ daml/array.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class array
+{
+public:
+    array(); // default constructor
+    array(const int size); // constructor
+};
